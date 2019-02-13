d030c126c1a6e7131737552280092ed2e30e4a29
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-13 11:54:32 +0100

    Basic react app exists

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Welcome to FreeBay, an open source alternative to eBay!
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
