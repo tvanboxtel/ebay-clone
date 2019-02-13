0c767e42646745f487ea575d9180e8e112e39461
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-13 13:57:11 +0100

    State now logged by rendering component

--- client/src/components/Advertisements/Advertisements.js
+++ client/src/components/Advertisements/Advertisements.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default (props) => {
-    console.log('My props are:', props)
+    console.log('My props are:', props.advertisements)
     return (
         <div className="App">
         <header className="App-header">
           Welcome to FreeBay, an open source alternative to eBay!
+
+          
         </header>
       </div>
     )
