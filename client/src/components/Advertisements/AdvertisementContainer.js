2d4b9efb455ef264d9251aef6e8e6b4957e531d0
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-13 14:29:45 +0100

    Page now renders ads in a list

--- client/src/components/Advertisements/AdvertisementContainer.js
+++ client/src/components/Advertisements/AdvertisementContainer.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux'
 
 
 class AdvertisementContainer extends React.Component {
-    // all kinds of extra stuff
     componentDidMount() {
         this.props.loadAdvertisements()
     }
--- client/src/components/Advertisements/Advertisements.js
+++ client/src/components/Advertisements/Advertisements.js
@@ -1,14 +1,32 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
 
-export default (props) => {
-    console.log('My props are:', props.advertisements)
+class Advertisements extends React.Component {
+
+  renderAds(ad) {
+    //can implement Link logic here
+    return <li key={ad.id}>{ad.title}, {ad.price}
+    </li>
+
+  }
+
+  render() {
+    // console.log('My props are:', this.props.advertisements)
+    const ads = this.props.advertisements
     return (
-        <div className="App">
+      <div className="App">
         <header className="App-header">
           Welcome to FreeBay, an open source alternative to eBay!
-
-          
+          {!ads && 'Loading...'}
+          {ads &&
+            <ul>
+              {ads.map(this.renderAds)}
+            </ul>}
         </header>
       </div>
     )
+  }
+
 }
+
+export default Advertisements
