265907dc44218fb09c7488a72b30168cd48fc8a6
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-13 12:36:37 +0100

    Refactored Advertisements component

---
+++ client/src/components/Advertisements/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Advertisements from './Advertisements'
+
+class AdvertisementContainer extends React.Component {
+    // all kinds of extra stuff
+    
+    state = {}
+
+    render() {
+        return <Advertisements />
+    }    
+}
+
+export default AdvertisementContainer
