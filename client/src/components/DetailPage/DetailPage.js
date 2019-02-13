5c4eb57c6f042a5b9e12515e4adcd7065de9d9c5
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-13 16:38:36 +0100

    Render page details, sans details

--- client/src/components/DetailPage/DetailPage.js
+++ client/src/components/DetailPage/DetailPage.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
 export default class DetailPage extends React.Component {
+    
     render() {
-        // if (this.props) console.log(this.props)
+
+        // would be nice to use destructive assignment here,
+        // have no time for it.
+
+        // if (this.props.advertisement) {
+        //     const { id, title, price } = this.props.advertisement
+        //     console.log(title)
+        // }
+
+        // Also, this is where the details of the product would go
+        // but there are no details in the database yet..
+
         return (
             <div>
-                Hello world!
+                {!this.props.advertisement && 'Loading...'}
+
+                {this.props.advertisement && <div>
+                    <h1>{this.props.advertisement.title}</h1>
+                    <h3>{this.props.advertisement.price}</h3>
+                </div>}
             </div>
         )
-    }
+    }    
 }
